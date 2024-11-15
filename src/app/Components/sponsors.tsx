ef0540fa116e7e3c02366f24ac8b534281e77be7
Uzair Ahmed <essani.addy@gmail.com>
2024-11-15 17:39:07 +0500

    Final

--- src/app/Components/sponsors.tsx
+++ src/app/Components/sponsors.tsx
@@ -3,12 +3,14 @@ import Image from "next/image";
 export default function Sponsors() {
   return (
     <div>
-      <div className="h-[538px] w-[100%] px-[50px] content-center text-white ">
+      <div className="h-[538px] w-[100%] px-[50px] content-center text-black ">
         <section className="justify-items-center">
           <h1 className=" text-[64px] font-bold tracking-tight text-center">
-            Our sponsors{" "}
+            Our sponsors
           </h1>
-          <div className="w-[1372px] flex justify-around items-center pt-10">
+          <br />
+          <br />
+          <div className="w-[1372px] flex justify-around items-center">
             <Image src="/apple.png" alt="arrow" width="55" height="68" />
             <Image src="/microsoft.png" alt="arrow" width="287" height="68" />
             <Image src="/slack.png" alt="arrow" width="280" height="68" />
